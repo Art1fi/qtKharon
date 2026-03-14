46fc7683f3ab4bafc8a8f2d8379c3cc98ab277ed
Art1fi <morp4ik@gmail.com>
2026-03-14 13:27:32 +0300

    Redesign startup flow and apply unified Qt stylesheet

--- logindialog.h
+++ logindialog.h
@@ -12,26 +12,33 @@ class LoginDialog : public QDialog
     Q_OBJECT
 
 public:
+    enum class StartupMode {
+        OpenExisting,
+        CreateNew
+    };
+
     explicit LoginDialog(QWidget *parent = nullptr);
     ~LoginDialog();
 
-    QString getPassword();
-    QString selectedPath;
-    QString getDatabasePath();
+    QString getPassword() const;
+    QString getDatabasePath() const;
+    StartupMode getMode() const;
 
 private slots:
-    void on_loginButton_clicked();
-
-    void on_fileButton_clicked();
-
-    void on_createButton_clicked();
+    void on_openExistingButton_clicked();
+    void on_createNewButton_clicked();
+    void on_choosePathButton_clicked();
+    void on_confirmButton_clicked();
+    void on_backButton_clicked();
 
 private:
     Ui::LoginDialog *ui;
 
-    void loginClicked();
+    QString selectedPath;
+    StartupMode mode;
 
+    void updateAuthPageTexts();
+    bool validateForm();
 };
 
 #endif // LOGINDIALOG_H
-
--- main.cpp
+++ main.cpp
@@ -3,29 +3,40 @@
 #include "databasemanager.h"
 
 #include <QApplication>
+#include <QFile>
+#include <QMessageBox>
+#include <QTextStream>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    DatabaseManager dbManager;
-    MainWindow w(&dbManager);
-    LoginDialog l;
-    QString path = "";
-    qDebug() << QSqlDatabase::drivers();
 
-
-    if (l.exec() == QDialog::Accepted) {
-        QString password = l.getPassword();
-        path = l.getDatabasePath();
-        if (dbManager.openDatabase(path, password)) {
-            w.show();
-            w.MainWindow::refreshTable();
-
-        }
-        else {
-            return 0;
-        }
+    QFile styleFile(":/styles/app.qss");
+    if (styleFile.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        QTextStream stream(&styleFile);
+        a.setStyleSheet(stream.readAll());
     }
+
+    DatabaseManager dbManager;
+    LoginDialog loginDialog;
+
+    if (loginDialog.exec() != QDialog::Accepted) {
+        return 0;
+    }
+
+    const QString password = loginDialog.getPassword();
+    const QString path = loginDialog.getDatabasePath();
+
+    if (!dbManager.openDatabase(path, password)) {
+        QMessageBox::critical(nullptr,
+                              "Ошибка",
+                              "Не удалось открыть базу данных. Проверьте путь и пароль.");
+        return 0;
+    }
+
+    MainWindow window(&dbManager);
+    window.show();
+    window.refreshTable();
 
     return a.exec();
 }
